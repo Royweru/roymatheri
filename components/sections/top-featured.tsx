fed16d729e5486581f47b17e1e3ff302577da4c5
Royweru <royezi17@gmail.com>
2025-05-21 09:07:35 +0300

    Initialized the top featured section

---
+++ components/sections/top-featured.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const TopFeatured = () => {
+  return (
+    <section id="top-featured" className=" py-14 bg-gray-50 dark:bg-gray-800">
+      <div className='max-w-6xl mx-auto px-4 sm:px-5'>
+        <h2 className='text-3xl md:text-4xl font-bold text-center text-gray-900 dark:text-white mb-8'>
+            Top Featured
+        </h2>
+      </div>
+    </section>
+  )
+}
